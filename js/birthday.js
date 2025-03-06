361a71ee1c3e4a51872574b622b57dabdd5700bd
xGRID <34432992+xGRIDev@users.noreply.github.com>
2025-03-06 13:06:55 +0000

    UPDATE [ PARTICLE EFFECT / GRADIENT BACKGROUND ]

--- js/birthday.js
+++ js/birthday.js
@@ -1,17 +1,25 @@
 window.addEventListener("load", () => {
+  let timerInterval;
   Swal.fire({
-    title: "Coba Tap/Click button Background Music nya dibawah ini Sayank",
+    title:
+      '<small> Hai Sayank <i class="bi bi-suit-heart-fill"></i>, Tap button dibawah ini dulu yaa </small> ',
+    timer: 100000,
+    timerProgressBar: true,
+    confirmButtonText: `
+    <i class="bi bi-play"></i> Play 
+  `,
+    confirmButtonColor: "#3085d6",
     showClass: {
       popup: `
           animate__animated
-          animate__fadeInUp
+          animate__fadeInDown
           animate__faster
         `,
     },
     hideClass: {
       popup: `
           animate__animated
-          animate__fadeOutDown
+          animate__fadeOutUp
           animate__faster
         `,
     },
@@ -21,6 +29,123 @@ window.addEventListener("load", () => {
       animationTimeline();
     } else {
       animationTimeline();
+      Swal.showLoading();
+      const timer = Swal.getPopup().querySelector("b");
+      timerInterval = setInterval(() => {
+        timer.textContent = `${Swal.getTimerLeft()}`;
+      }, 100);
     }
+  });
+  tsParticles.load("particles", {
+    fpsLimit: 60,
+    backgroundMode: {
+      enable: true,
+      zIndex: -1,
+    },
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          area: 800,
+        },
+      },
+      color: {
+        value: [
+          "#3998D0",
+          "#2EB6AF",
+          "#A9BD33",
+          "#FEC73B",
+          "#F89930",
+          "#F45623",
+          "#D62E32",
+        ],
+      },
+      destroy: {
+        mode: "split",
+        split: {
+          count: 1,
+          factor: {
+            value: 5,
+            random: {
+              enable: true,
+              minimumValue: 4,
+            },
+          },
+          rate: {
+            value: 10,
+            random: {
+              enable: true,
+              minimumValue: 5,
+            },
+          },
+          particles: {
+            collisions: {
+              enable: false,
+            },
+            destroy: {
+              mode: "none",
+            },
+            life: {
+              count: 1,
+              duration: {
+                value: 1,
+              },
+            },
+          },
+        },
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          sides: 5,
+        },
+      },
+      opacity: {
+        value: 1,
+        random: false,
+        animation: {
+          enable: false,
+          speed: 1,
+          minimumValue: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 8,
+        random: {
+          enable: true,
+          minimumValue: 4,
+        },
+        animation: {
+          enable: false,
+          speed: 40,
+          minimumValue: 0.1,
+          sync: false,
+        },
+      },
+      collisions: {
+        enable: true,
+        mode: "destroy",
+      },
+      move: {
+        enable: true,
+        speed: 7,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    detectRetina: true,
   });
 });
